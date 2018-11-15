00806acea76afdeb23e9be1014a4038cb29a78ae
Supuni Nimeshika <Supuni.Nimeshika@gmail.com>
2018-11-15 20:46:42 +0530

    Add Class Related To String

---
+++ src/String/DuplicateCharactersInString.java
@@ -0,0 +1,24 @@
+package String;
+
+/**
+ * Created by Supuni Nimeshika on 11/15/2018.
+ */
+public class DuplicateCharactersInString {
+
+    public static void main(String argu[]) {
+
+        String str = "w3schools";
+        int cnt = 0;
+        char[] inp = str.toCharArray();
+        System.out.println("Duplicate Characters are:");
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j < str.length(); j++) {
+                if (inp[i] == inp[j]) {
+                    System.out.println(inp[j]);
+                    cnt++;
+                    break;
+                }
+            }
+        }
+    }
+}
